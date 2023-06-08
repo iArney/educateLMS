0ccc3e841e6c6ee19b3936267913bd4603400cc7
Arnold <86104635+iArney@users.noreply.github.com>
2023-06-08 16:50:31 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn .25s ease-in',
+        slideDown: 'slideDown 0.3s ease-in',
+      },
+    },
+    backgroundImage:{
+      "teachBg": "url('/src/assets/teach.svg')",
+      "loginBg": "url('/src/assets/desktop.jpg')",
+      "registerBg": "url('/src/assets/register.png')"
+    }
+  },
+  plugins: [],
+}
